01b2502863d451da21f73332f0b0b3cd84d0996d
Henry Li <2001henryli@gmail.com>
2015-06-30 17:14:13 -0700

    Updated linkdrive
    added new servo command for backwards

--- Double Elimination/LinkDrive/drive.h
+++ Double Elimination/LinkDrive/drive.h
@@ -11,7 +11,7 @@
 #define PI 3.14159265358979
 
 #define SPD 100//turning
-#define SPDl 77//left forward
+#define SPDl 83//left forward
 #define SPDr 100//right forward
 #define rdistmult 1.0
 #define SPDlb 80.//left backward
